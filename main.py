95946f56698e64c25d93b2b1eecc077c66e180ce
Freddykaseya <freddykaseya@gmail.com>
2024-10-05 15:28:32 +0100

    gestion d'âge

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello word ")
+nom = input("entrez votre nom :")
+age = int(input("entrez votre age :"))
+if age < 18 :
+    print("vous n'êtes pas eligible à ce site") 
+else :
+    print(nom + "bienvenue à ce site")
+    
