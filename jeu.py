2ff21300a402f0fade8948e065c33fdb20cacf05
Freddykaseya <freddykaseya@gmail.com>
2024-10-05 23:41:13 +0100

    modification du jeu

--- jeu.py
+++ jeu.py
@@ -1,45 +1,49 @@
 import random
-gamer1_name = input("entrez votre nom ")
+gamer1_name = input("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tentrez votre nom : ")
 gamer2_name = "ordinateur"
-life_gamer1 = 100
-life_gamer2 = 100
-outils = ["arme","epée","bouclier"]
-print("voici vos outils de combat :")
-print("arme = 5, epée = 2 , bouclier")
-print(f"{gamer1_name} votre adversaire est {gamer2_name}")
+life_gamer1 = 15
+life_gamer2 = 15
+print("\t\t\t\t\t\t\t\t\t\t\t\t\tvoici vos outils de combat et leurs cout:")
+print("\t\t\t\t\t\t\t\t\t\t\t\t\tarme = 5 vies")
+print("\t\t\t\t\t\t\t\t\t\t\t\t\tepée = 2  vie" )
+print("\t\t\t\t\t\t\t\t\t\t\t\t\tbouclier = 2 chance")
+print(f"\t\t\t\t\t\t\t\t\t\t\t\t\t\t{gamer1_name} votre adversaire est {gamer2_name}")
 def gamer1(option):
-   life_gamer2 = 100
+   global life_gamer2 
+   life_gamer2 = 15
    life_gamer2 -= option
-   print(f"life game2  = {life_gamer2}" )
+   print(f"vie de l'ordinateur = {life_gamer2}")
    return 
 
 def gamer2():
-    life_gamer1 = 100
+    global life_gamer1
+    life_gamer1 = 15
     number = random.randint(3,7)
     life_gamer1 -= number
-    print(f"voici le vie de l'ordinateur = {life_gamer1}")
+    print(f"vie de  {gamer1_name} =  {life_gamer1}")
     return life_gamer1
-
-while(life_gamer1>0 and life_gamer2>0):
-    def round_gamer1():
+def start_game():  
+    while(life_gamer1>0 and life_gamer2>0):
         print(" 1 = arme , 2 = épee , 3 = bouclier ")
         outil = int(input("entrez votre choix : "))
-        while outil == 2 and outil == 5:
-            if outil == 1:
-                gamer1(5)
-                print("joeur 2 ")
-                gamer2() 
-            elif(outil == 2): 
-                 gamer1(3)
-                 print("joeur 2 ")
-                 gamer2()   
-            elif(outil == 3 ):
-                bouclier = 2
-                if(bouclier == 3):
-                    print("plus de bouclier :")
-                    gamer2()
-        return
-round_gamer1()
+        if outil == 1:
+            gamer1(5)
+            gamer2() 
+        elif(outil == 2): 
+            gamer1(3)
+            gamer2()   
+        elif(outil == 3 ):
+            bouclier = 2
+            if(bouclier < 3):
+                print("plus de bouclier :")
+                gamer1(0)
+                gamer2()
+            else:
+                continue
+        else:
+            print("fin")
+start_game()
+        
 
 
 
