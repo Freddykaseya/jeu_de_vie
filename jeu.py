4b00d9eee25cc4bc42eedb85cbba712e1d49f0c6
Freddykaseya <freddykaseya@gmail.com>
2024-10-07 03:00:19 +0100

    amelioration du jeu

--- jeu.py
+++ jeu.py
@@ -1,47 +1,63 @@
 import random
-gamer1_name = input("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tentrez votre nom : ")
-gamer2_name = "ordinateur"
-life_gamer1 = 15
-life_gamer2 = 15
-print("\t\t\t\t\t\t\t\t\t\t\t\t\tvoici vos outils de combat et leurs cout:")
-print("\t\t\t\t\t\t\t\t\t\t\t\t\tarme = 5 vies")
-print("\t\t\t\t\t\t\t\t\t\t\t\t\tepée = 2  vie" )
-print("\t\t\t\t\t\t\t\t\t\t\t\t\tbouclier = 2 chance")
-print(f"\t\t\t\t\t\t\t\t\t\t\t\t\t\t{gamer1_name} votre adversaire est {gamer2_name}")
-def gamer1(option):
-   global life_gamer2 
-   life_gamer2 = 15
-   life_gamer2 -= option
-   print(f"vie de l'ordinateur = {life_gamer2}")
-   return 
 
-def gamer2():
-    global life_gamer1
+gamer1_name = input("\tentrez votre nom : ")
+gamer2_name = "ordinateur"
+print(f"\t{gamer1_name} votre adversaire est {gamer2_name} ")
+print("vous avez chacun 15 vie ")
+
+def attack():
+    print("\tchoisissez votre attaque:")
+    print("\t1. arme = 5 vies")
+    print("\t2. epée = 2  vies" )
+    print("\t3. bouclier = 2 chances")
+    
+def start_game(): 
     life_gamer1 = 15
-    number = random.randint(3,7)
-    life_gamer1 -= number
-    print(f"vie de  {gamer1_name} =  {life_gamer1}")
-    return life_gamer1
-def start_game():  
-    while(life_gamer1>0 and life_gamer2>0):
-        print(" 1 = arme , 2 = épee , 3 = bouclier ")
-        outil = int(input("entrez votre choix : "))
-        if outil == 1:
-            gamer1(5)
-            gamer2() 
-        elif(outil == 2): 
-            gamer1(3)
-            gamer2()   
-        elif(outil == 3 ):
-            bouclier = 2
-            if(bouclier < 3):
-                print("plus de bouclier :")
-                gamer1(0)
-                gamer2()
+    life_gamer2 = 15 
+    protection = 2
+
+    print(f"{gamer1_name} et {gamer2_name} bienvenue dans le jeu de rôle")
+
+    while life_gamer1>0 and life_gamer2>0 :
+
+        attack_gamer2 = random.randint(3,7) 
+
+        attack()
+        tools = int(input("entrez votre choix : "))
+
+        if tools == 1:
+            life_gamer2 -=5
+            print(f"gamer {gamer2_name}= {life_gamer2}")
+            life_gamer1 -= attack_gamer2
+            print(f"gamer {gamer1_name} votre vie = {life_gamer1}")
+           
+        elif tools == 2: 
+            life_gamer2 -= 3
+            print(f"gamer {gamer2_name}= {life_gamer2}")
+            life_gamer1 -= attack_gamer2
+            print(f"gamer {gamer1_name} votre vie = {life_gamer1}")
+        elif tools == 3 :
+            if(protection < 3):
+                protection -= 1
+                print(f"vous avez utilisé le bouclier il vous reste : {protection}")
+            elif protection == 0:
+                life_gamer1 -= attack_gamer2
+                print(f"{gamer1_name} vous n'avez plus de bouclier et vous avez perdue {attack_gamer2}")
+                print(f"gamer {gamer2_name}= {life_gamer2}")
+                print(f"gamer {gamer1_name} votre vie = {life_gamer1}")
             else:
-                continue
-        else:
-            print("fin")
+                print("vous n'avez plus de bouclier")
+                life_gamer1 -= attack_gamer2
+                print(f"gamer {gamer2_name} votre vie = {life_gamer2}")
+                print(f"gamer {gamer1_name} votre vie = {life_gamer1}")
+        else : 
+            print("choix invalide")
+        print("****************************************************")
+    if life_gamer1 <= 0 :
+        print(f"GAME OVER et le gamer {gamer2_name} gagne avec = {life_gamer2} vie")
+        
+    else:
+        print(f"Bravooooo, {gamer1_name} game avec {life_gamer1} contre {life_gamer2} de vie")
 start_game()
         
 
